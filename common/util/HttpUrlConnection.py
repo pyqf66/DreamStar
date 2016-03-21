2b2fb97721acff228b3042d3242710a53711c529
pyqf66 <zyf3282@msn.cn>
2016-03-21 14:47:44 +0800

    更新http请求模块

--- common/util/HttpUrlConnection.py
+++ common/util/HttpUrlConnection.py
@@ -1,123 +1,82 @@
-# coding=utf-8
-###########################################
-# File: HttpUrlConnection.py
-# Desc: http接口工具类
-# Author: zhangyufeng
-# History: 2015/11/15 zhangyufeng 新建
-###########################################
-import http
-from http import client
-import urllib
-import json
-import simplejson
+# -*- coding:utf-8 -*-
+import requests
 import logging
 
 
 class HttpUrlConnection(object):
-    u''' 
-        methon为请求方法
-        parameter为请求参数
-        cookie为获取的cookie
-        headers为请求头
+    '''
+    :function __init__: 构造方法
+    :function request: 不带cookie的请求
+    :function request_with_cookies: 带cookie的请求
     '''
 
     global logger
     logger = logging.getLogger("DreamStar.app")
 
     # 处理预置数据
-    def __init__(self, url, method="GET", parameters=None, cookie=None, headers={}):
-        try:
-            # 解析url
-            if type(url) == bytes:
-                self.__url = url.decode("utf-8")
-            if type(url) == str:
-                self.__url = url
-            logger.debug(self.__url)
-            scheme, rest = urllib.parse.splittype(self.__url)
-            # 拆分域名和路径
-            logger.debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-            self.__host_absolutely, self.__path = urllib.parse.splithost(rest)
-            host_list = self.__host_absolutely.split(":")
-            if len(host_list) == 1:
-                self.__host = host_list[0]
-                self.__port = 80
-            elif len(host_list) == 2:
-                self.__host = host_list[0]
-                self.__port = host_list[1]
-            # 对所传参数进行处理
-            self.__method = method
-            self.__data = parameters
-            self.__cookie = cookie
-            if parameters != None:
-                self.__parameters_urlencode_deal = urllib.parse.urlencode(parameters)
+    def __init__(self, url=None, method="GET", parameters=None, cookies=None, headers={}, get_cookie_url=None,
+                 get_cookie_method=None, get_cookie_request_data=None,
+                 get_cookie_headers=[('User-agent', 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)')]):
+        '''
+        :param url: 请求使用的url，由于发送cookie的请求方法可以单独传url故此参数非必填。使用request方法请求时必填。
+        :param method: 请求的方法
+        :param parameters: 请求的参数
+        :param cookie: cookie
+        :param headers: 请求头
+        :param get_cookie_url: 获取cookie的url,如果调用request_with_cookies时此参数必须传
+        :param get_cookie_request_data: 获取cookie时需要传的参数
+        :param get_cookie_headers: 获取cookie时需要加的请求头
+        '''
+        self.__url = url
+        self.__method = method
+        self.__parameters = parameters
+        self.__cookies = cookies
+        self.__headers = headers
+        self.__get_cookie_url = get_cookie_url
+        self.__get_cookie_method = get_cookie_method
+        self.__get_cookie_request_data = get_cookie_request_data
+        self.__get_cookie_headers = get_cookie_request_data
+        self.__session_object = requests.Session()
+        if self.__get_cookie_url is not None:
+            if self.__get_cookie_method == "GET":
+                response_oject_with_cookie = self.__session_object.get(url=self.__get_cookie_url,
+                                                                       data=self.__get_cookie_request_data)
             else:
-                self.__parameters_urlencode_deal = ""
-            self.__jdata = simplejson.dumps(parameters, ensure_ascii=False)
-            self.__headers = headers
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
+                response_oject_with_cookie = self.__session_object.post(url=self.__get_cookie_url,
+                                                                        data=self.__get_cookie_request_data,
+                                                                        headers=self.__get_cookie_headers)
 
-    # 发送普通请求,要求完全满足http协议规则
+    # 发送普通请求
     def request(self):
         try:
-            conn = client.HTTPConnection(self.__host, self.__port)
             if self.__method == "GET":
-                self.path = self.__path + self.__parameters_urlencode_deal
-                conn.request(self.__method, self.__path)
-            if self.__method == "POST":
+                response = requests.get(url=self.__url, params=self.__parameters)
+            else:
                 if self.__headers == {"Content-type": "application/json"}:
-                    conn.request(self.__method, self.__path, self.__jdata, self.__headers)
-                if self.__headers == {"Content-type": "application/x-www-form-urlencoded"}:
-                    conn.request(self.__method, self.__path, self.__data, self.__headers)
+                    response = requests.post(url=self.__url, json=self.__parameters)
+                else:
+                    response = requests.post(url=self.__url, data=self.__parameters)
+            return response
+        except:
+            logger.exception("发送普通请求错误:")
 
-            response = conn.getresponse()
-            result_origin = response.read()
-            try:
-                result = unicode(result_origin, "gb2312").encode("utf8")
-            except:
-                result = result_origin
-            return result
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
-
-    # 使用urllib.request发送带cookie的请求
+    # 使用requests发送带cookies的请求
     def request_with_cookies(self):
         try:
-            cookiejar = http.CookieJar()
-            cookiejar.set_cookie(self.__cookie)
-            opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookiejar))
-            if self.__data == None:
-                request = urllib.request.Request(self.__url)
+            if self.__method == "GET":
+                response = self.__session_object.get(url=self.__url, params=self.__parameters)
             else:
-                request = urllib.request.Request(self.__url, self.__data)
-            html = opener.open(request).read()
-            return html
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
+                if self.__headers == {"Content-type": "application/json"}:
+                    response = self.__session_object.post(url=self.__url, json=self.__parameters)
+                else:
+                    response = self.__session_object.post(url=self.__url, data=self.__parameters)
+            return response
+        except:
+            logger.exception("带cookies请求错误：")
 
-    # 提供外部host数据    
-    def get_host(self):
+    # 获得带cookie对象
+    def get_session_object(self):
         try:
-            return self.__host
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
-
-    # 提供外部path数据
-    def get_path(self):
-        try:
-            return self.__path
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
-
-    # 提供外部经过ulrencode处理后的数据
-    def get_parameters_urlencode_deal(self):
-        try:
-            return self.__parameters_urlencode_deal
-        except Exception as e:
-            logger.error(e)
-            logger.exception(u"捕获到错误如下:")
+            return self.__session_object
+        except:
+            logger.exception("获取带cookie对象错误")
